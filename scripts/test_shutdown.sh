14747c7fa29789d555debaaa56287b650ecf4a89
Dan Petrisko <petrisko@cs.washington.edu>
2024-06-22 22:34:56 -0700

    Adding poweroff force

--- scripts/test_shutdown.sh
+++ scripts/test_shutdown.sh
@@ -5,7 +5,8 @@ echo "[bsg] Running test_shutdown.sh with argument $1"
 case "$1" in
     'start')
         echo "[bsg] About to poweroff"
-        poweroff
+        poweroff -f -d 3
+        sleep 5
         ;;
     'stop')
         echo "[bsg] test_shutdown.sh stop -> null"
